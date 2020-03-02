dbcba729bd71f4701ae290f10f1779b71a10ba3c
Fernando J. Spitz <fjspitz@gmail.com>
2020-03-02 07:38:43 -0300

    Commit inicial

---
+++ src/main/java/ar/com/fjs/springbatchdashboard/SpringBatchDashboardApplication.java
@@ -0,0 +1,13 @@
+package ar.com.fjs.springbatchdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/fjs/springbatchdashboard/SpringBatchDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.fjs.springbatchdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
